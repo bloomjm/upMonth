dc741995cc04b0bf0ecf0f75286414c5371eeda2
bloomjm <jbloom@greenlakecapital.com>
2014-02-13 18:04:45 -0500

    20140213180445

--- js/mailingList.js
+++ js/mailingList.js
@@ -25,3 +25,11 @@
             $( ".signin" ).hide();
        });
     });
+    
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 500);
+        return false;
+    });
+    
